f75e695ab7ec6ca6783803e9da5713e316b455b5
György Csadó <gyorgy.csado@gmail.com>
2016-03-16 10:15:35 +0100

    bugfix

--- final-exam.js
+++ final-exam.js
@@ -3,17 +3,17 @@
 var fs = require('fs');
 
 function wrietObjectToFile(filename, object, callback) {
-  setTimeout(write, 2000);
-  function write(filename, object, callback) {
-    var json_data = JSON.stringify(object);
-    fs.writeFile(filename, json_data, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      callback();
-    });
-  }
+  setTimeout(write(filename, object, callback), 2000);
 }
 
+function write(filename, object, callback) {
+  var json_data = JSON.stringify(object);
+  fs.writeFile(filename, json_data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    callback();
+  });
+}
 
 module.exports = wrietObjectToFile;
